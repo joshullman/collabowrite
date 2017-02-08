65a781b81da48c3ebe95c63816b40897ab675fc0
Josh Ullman <joshullman@gmail.com>
2017-02-07 17:09:01 -0800

    successfully seeded database

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,15 @@ append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 namespace :deploy do
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-  end
+	desc "reload the database with seed data"
+	task :seed do
+		on "collabowrite@159.203.114.32" do
+	    execute "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=production"
+	  end
+	end
 end
+
+after :deploy, "deploy:seed"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
