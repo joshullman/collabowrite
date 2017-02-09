801bfeb04a9127d950353fcf61f6dd1df8f25d4d
Josh Ullman <joshullman@gmail.com>
2017-02-09 12:58:43 -0800

    add set_user to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@users = User.all
 	end
 
 	def show
-    @user = User.find(params[:id])
   end
 
   private
 
+  def set_user
+  	@user = User.find(params[:id])
+  end
+
   def user_params
-  	params.require(:user).permit(:id, :name, :avatar)
+  	params.require(:user).permit(:name, :avatar)
   end
 end
