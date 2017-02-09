e3c12e4709b61a5c5f6259cb459b5f642d8f15d8
Josh Ullman <joshullman@gmail.com>
2017-02-09 12:49:58 -0800

    add changes to users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  	params.require(:user).permit(:name, :avatar)
+  	params.require(:user).permit(:id, :name, :avatar)
   end
 end
