949204f9ba9b4729c8a1c3f1dbac017acbe802f0
Josh Ullman <joshullman@gmail.com>
2017-02-28 11:14:26 -0800

    add buttons bar to profile page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,55 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+
+	$('.scripts_button').click(function(){
+		$('.scripts_button').css("text-decoration", "underline");
+		$('.groups_button').css("text-decoration", "none");
+		$('.members_button').css("text-decoration", "none");
+		$('.comments_button').css("text-decoration", "none");
+		$('.scripts_container').show();
+    $('.groups_container').hide();
+    $('.members_container').hide();
+    $('.commeents_container').hide();
+  });
+
+	$('.groups_button').click(function(){
+		$('.scripts_button').css("text-decoration", "none");
+		$('.groups_button').css("text-decoration", "underline");
+		$('.members_button').css("text-decoration", "none");
+		$('.comments_button').css("text-decoration", "none");
+		$('.scripts_container').hide();
+    $('.groups_container').show();
+    $('.members_container').hide();
+    $('.comments_container').hide();
+  });
+
+	$('.members_button').click(function(){
+		$('.scripts_button').css("text-decoration", "none");
+		$('.groups_button').css("text-decoration", "none");
+		$('.members_button').css("text-decoration", "underline");
+		$('.comments_button').css("text-decoration", "none");
+		$('.scripts_container').hide();
+    $('.groups_container').hide();
+    $('.members_container').show();
+    $('.comments_container').hide();
+  });
+
+	$('.comments_button').click(function(){
+		$('.scripts_button').css("text-decoration", "none");
+		$('.groups_button').css("text-decoration", "none");
+		$('.members_button').css("text-decoration", "none");
+		$('.comments_button').css("text-decoration", "underline");
+		$('.scripts_container').hide();
+    $('.groups_container').hide();
+    $('.members_container').hide();
+    $('.comments_container').show();
+  });
+
+});
