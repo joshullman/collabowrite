68a23120b3773e8c6ee122fe8d2f69dd1bff3732
Josh Ullman <joshullman@gmail.com>
2017-03-01 13:00:03 -0800

    add functional comment field to script/show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,27 @@
 class CommentsController < ApplicationController
   
   def create
-    @comment = Comment.new(comment_params)
-
+    if params[:script_id]
+      @script = Script.find(params[:script_id])
+      @comment = @script.comments.new(comment_params)
+    else
+      @group = Group.find(params[:group_id])
+      @comment = @group.comments.new(comment_params)
+    end
+    @comment.user_id = current_user.id
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        if params[:script_id]
+          format.html { redirect_to @script, notice: 'Comment was successfully created.' }
+        else
+          format.html { redirect_to @group, notice: 'Comment was successfully created.' }
+        end
       else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        if params[:script_id]
+          format.html { redirect_to @script, notice: 'Comment did not save for some mysterious reason.' }
+        else
+          format.html { redirect_to @group, notice: 'Comment did not save for some mysterious reason.' }
+        end
       end
     end
   end
