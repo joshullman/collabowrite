e54416e9b527fde31e43360c39a347a8c3f70b39
Josh Ullman <joshullman@gmail.com>
2017-03-06 12:21:02 -0800

    slightly modify model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many   :scripts
   has_many   :group_users
@@ -41,7 +42,6 @@ class User < ApplicationRecord
     @avatar_remote_url = url_value
   end
 
-  devise :omniauthable, :omniauth_providers => [:facebook]
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
