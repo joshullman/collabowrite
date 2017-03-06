bd767f74de90d28a2b492192b1b200b419e8a78c
Josh Ullman <joshullman@gmail.com>
2017-03-06 15:51:57 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170303224504) do
     t.string   "username"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "photo_url"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.string   "avatar_file_name"
