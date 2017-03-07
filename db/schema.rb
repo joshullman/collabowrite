7998ff94abb428a4d9a136ac5e6c23a09bba03d8
Josh Ullman <joshullman@gmail.com>
2017-03-06 17:11:46 -0800

    minor changes"

--- db/schema.rb
+++ db/schema.rb
@@ -69,14 +69,13 @@ ActiveRecord::Schema.define(version: 20170303224504) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
+    t.string   "email",            default: "", null: false
     t.string   "username"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.string   "photo_url"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.string   "provider"
     t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
